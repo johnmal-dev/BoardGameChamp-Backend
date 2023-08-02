158c10296f9092b784813f65ef78b5a374c0cfd2
John Malapit <johncmalapit@gmail.com>
2023-08-02 18:00:29 -0400

    define basic migrations, models and factories

--- database/migrations/2023_08_02_214419_create_game_results_table.php
+++ database/migrations/2023_08_02_214419_create_game_results_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('game_results', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('avatar')->nullable();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('game_id');
+            $table->foreignId('user_id');
+            $table->integer('rank');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('game_results');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Game;
+use App\Models\Player;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,60 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $jc = User::factory()->create([
+            'name' => 'John Malapit',
+            'username' => 'johnmal',
+            'email' => 'jc@mail.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $keno = User::factory()->create([
+            'name' => 'Keno Aguiar',
+            'username' => 'yellowboi',
+            'email' => 'keno@mail.com',
+        ]);
+
+        $jeremy = User::factory()->create([
+            'name' => 'Jeremy Aguiar',
+            'username' => 'jayremy',
+            'email' => 'jeremy@mail.com',
+        ]);
+
+        $ryan = User::factory()->create([
+            'name' => 'Ryan Cortez',
+            'username' => 'ryry',
+            'email' => 'ryan@mail.com',
+        ]);
+
+        $kevin = User::factory()->create([
+            'name' => 'Kevin Bautista',
+            'username' => 'kb',
+            'email' => 'kevin@mail.com',
+        ]);
+
+        $augy = User::factory()->create([
+            'name' => 'Augy Lagundino',
+            'username' => 'awgeeeee',
+            'email' => 'augy@mail.com',
+        ]);
+
+        $game = Game::factory()->create();
+
+        $player1 = Player::factory()->create([
+            'user_id' => $jc->id,
+            'game_id' => $game->id,
+            'rank' => 1,
+        ]);
+
+        $player2 = Player::factory()->create([
+            'user_id' => $keno->id,
+            'game_id' => $game->id,
+            'rank' => 2,
+        ]);
+
+        $player3 = Player::factory()->create([
+            'user_id' => $jeremy->id,
+            'game_id' => $game->id,
+            'rank' => 3,
+        ]);
     }
 }
