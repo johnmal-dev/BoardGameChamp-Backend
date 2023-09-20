c5ac783560b8c844741f3a372211731c325a07d1
John Malapit <johncmalapit@gmail.com>
2023-09-20 09:50:29 -0400

    rewrite play-results route using query builder

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,25 @@
 <?php
 
+use App\Models\PlayResult;
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 // get users
 Route::get('/users', function () {
-    return App\Models\User::all();
+    return App\Models\User::query()->orderBy('username')->get();
 });
 
 // get all game results with players, exclude id and timestamps
-Route::get('/game-results', function () {
-    return App\Models\GameResult::with(['players', 'game'])->get()->makeHidden(['id', 'created_at', 'updated_at']);
+Route::get('/play-results', function () {
+    return App\Models\PlayResult::with('players')->get();
+});
+
+// get all game results for a specific game
+Route::get('/play-results/{game_id}', function ($game_id) {
+    return DB::table('play_results')
+        ->join('players', 'play_results.id', '=', 'players.play_result_id')
+        ->join('users', 'players.user_id', '=', 'users.id')
+        ->get();
 });
