3bfb545d9da6015d56a03a54b102f71715c14dca
John Malapit <johncmalapit@gmail.com>
2024-10-03 16:18:26 -0400

    install backend inertia stuff

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventsController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('App');
 });
